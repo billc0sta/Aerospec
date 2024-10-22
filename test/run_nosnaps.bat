72a2ea7d50dc80ce9e29f853a6fad59c0154a1a7
Mohamed <billcosta222@gmail.com>
2024-10-22 03:12:11 +0300

    additional string testing

--- test/run_nosnaps.bat
+++ test/run_nosnaps.bat
@@ -3,7 +3,7 @@ SetLocal EnableExtensions DisableDelayedExpansion
 
 Set /A nosnaps=0
 
-For %%G In ("test\*") Do (
+For %%G In ("tests\*") Do (
 
     If Not Exist "snapshots\%%~nG.txt" (
        dune.exe exec ..\_build\default\bin\main.exe "%%G" 1>"results\%%~nG.txt" 2>NUL
@@ -13,4 +13,5 @@ For %%G In ("test\*") Do (
 )
 
 Echo.
+Echo SUMMARY: 
 Echo NOSNAP: %nosnaps%
